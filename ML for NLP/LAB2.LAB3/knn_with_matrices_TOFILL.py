caa4222e4733193254128703799e50bbeecdef7b
valjcq <valjcq@gmail.com>
2024-10-05 07:47:54 +0200

    Update

--- ML for NLP/LAB2.LAB3/knn_with_matrices_TOFILL.py
+++ ML for NLP/LAB2.LAB3/knn_with_matrices_TOFILL.py
@@ -28,9 +28,12 @@ class KNN:
     K = maximum number of neighbors to consider
 
     """
-    def __init__(self, X, Y, K=1, weight_neighbors=False, verbose=False):
+    def __init__(self, X, Y, K=1, weight_neighbors=False, verbose=False, cos_dist="similarity"):
         self.X_train = X   # (nbexamples, d)
         self.Y_train = Y   # list of corresponding gold classes
+
+
+        self.cos_dist = cos_dist
 
         # nb neighbors to consider
         self.K = K
@@ -39,30 +42,66 @@ class KNN:
         self.weight_neighbors = weight_neighbors
 
         self.verbose = verbose
+        
+        if verbose:
+            print(f"KNN classifier built with {K} neighbors")
+            print(f"Weighted neighbors: {weight_neighbors}")
+            print(f"Distance: {cos_dist}")
 
-    def cosine_distance(self, X1, X2):
+    def cosine_similarity_sorted(self, X1, X2):
         """
         Compute the cosine distance between two matrices X1 and X2
         """
         X1_norm = np.linalg.norm(X1, axis=1, keepdims=True)
         X2_norm = np.linalg.norm(X2, axis=1, keepdims=True)
         cos_dist = np.dot(X1 / X1_norm, (X2 / X2_norm).T)
+        cos_dist = np.array(cos_dist)
         return cos_dist
+
+    def calc_distance(self, X1, X2):
+        """
+        Compute the cosine distance between two matrices X1 and X2
+        """
+        X1_norm = np.linalg.norm(X1, axis=1, keepdims=True)
+        X2_norm = np.linalg.norm(X2, axis=1, keepdims=True)
+        cos_dist = np.dot(X1 / X1_norm, (X2 / X2_norm).T)
+        cos_dist = np.array(cos_dist)
+        return 1 - cos_dist
 
     def evaluate_on_test_set(self, X_test, Y_test, indices):
         """
         Evaluate the classifier on a test set
         """
-        cos_dist = self.cosine_distance(X_test, self.X_train)
-        # je dois prendre les 3 plus proches distances, en gardant les indices
-        cos_dist = np.array(cos_dist)
-        sorted_dist = cos_dist.argsort(axis=1)[:, -self.K:]
+        if self.cos_dist == "similarity":
+            sorted_dist = self.cosine_similarity_sorted(X_test, self.X_train)
+            print(f"sorted_dist: {sorted_dist}")
+            sorted_dist_indices = np.argsort(sorted_dist, axis=1)
+            print(f"sorted_dist_indices: {sorted_dist_indices}")
+        elif self.cos_dist == "distance":
+            sorted_dist = self.calc_distance(X_test, self.X_train)
+            sorted_dist_indices = np.argsort(sorted_dist, axis=1)
+        # je dois prendre les K plus proches distances, en gardant les indices
+        sorted_dist_indices = sorted_dist_indices[:, -self.K:]
         # [: , -self.K:] all lines, last K columns (the K nearest neighbors)
         predictions = []
-        for doc, neighbors in enumerate(sorted_dist):
-            neighbor_classes = []
-            neighbor_classes = [self.Y_train[idx] for idx in neighbors]
-            predicted_class = max(set(neighbor_classes), key=neighbor_classes.count)
+        print(f"sorted_dist_indices: {sorted_dist_indices}")
+        for doc, neighbors in enumerate(sorted_dist_indices):
+            neighbor_classes = {}
+            if self.weight_neighbors:
+                for idx in neighbors:
+                    if Y_train[idx] not in neighbor_classes:
+                        neighbor_classes[Y_train[idx]] = sorted_dist[doc, idx]
+                    else:
+                        neighbor_classes[Y_train[idx]] += sorted_dist[doc, idx]
+            else:
+                for idx in neighbors:
+                    if Y_train[idx] not in neighbor_classes:
+                        neighbor_classes[Y_train[idx]] = 1
+                    else:
+                        neighbor_classes[Y_train[idx]] += 1
+            print(f"Doc {doc} neighbors: {neighbor_classes}")
+            print(f"Neighbors_classes: {neighbor_classes}")
+            predicted_class = max(neighbor_classes, key=neighbor_classes.get)
             predictions.append(predicted_class)
         accuracy = np.mean([pred == real for pred, real in zip(predictions, Y_test)])
         print(f"Accuracy: {accuracy * 100:.2f}% ({accuracy * len(Y_test)}/{len(Y_test)})")
@@ -131,7 +170,7 @@ parser.add_argument('test_file', help='Examples\' file, used for evaluation', de
 parser.add_argument("-k", '--k', default=1, type=int, help='Maximum number of nearest neighbors to consider (all values between 1 and K will be tested). Default=1')
 parser.add_argument('-v', '--verbose',action="store_true",default=False,help="If set, triggers a verbose mode. Default=False")
 parser.add_argument('-w', '--weight_neighbors', action="store_true", default=False,help="If set, neighbors will be weighted when scoring classes. Default=False")
-
+parser.add_argument('-c', '--cos_dist', choices=['similarity', 'distance'], default="similarity", help='Choose between cosine similarity or cosine distance. Default=similarity')
 args = parser.parse_args()
 
 
@@ -142,10 +181,10 @@ args = parser.parse_args()
 
 train_examples = read_examples(args.train_file) # .dev.examples
 test_examples = read_examples(args.test_file) # .test.examples
+cos_dist = args.cos_dist # similarity or distance
 
 print(f"Read {len(train_examples.dict_vectors)} trained (.dev) examples") # On va baser notre KNN sur ces exemples
 print(f"Read {len(test_examples.dict_vectors)} test (.test) examples") # On va évaluer notre KNN sur ces exemples
-
 
 # ------------------------------------------------------------
 # Building indices for vocabulary in TRAINING examples
@@ -180,5 +219,6 @@ for k in range(1, args.k + 1):
                    Y=Y_train,
                    K=k,
                    weight_neighbors=args.weight_neighbors,
-                   verbose=args.verbose)
+                   verbose=args.verbose,
+                   cos_dist=cos_dist)
     accuracies = myclassifier.evaluate_on_test_set(X_test, Y_test, i2w)
